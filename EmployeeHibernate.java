e45b817da672db45d4c8ac6e85e2634831e6468c
ablenpowell <90283926+ablenpowell@users.noreply.github.com>
2021-10-18 00:55:40 -0500

    EmployeeHibernate.java

---
+++ EmployeeHibernate.java
@@ -0,0 +1,38 @@
+package net.javaguides.hibernate.util;
+
+import javax.persistence.Entity;  
+import javax.persistence.Id;  
+import javax.persistence.Table;  
+
+
+@Entity  
+@Table(name= "employee")   
+
+public class Employee {
+	
+	@Id   
+	private int EmpID;    
+	private String EmpName;  
+	private String EmpPassword;
+	    
+	public int getId() {    
+	    return EmpID;    
+	}    
+	public void setId(int id) {    
+	    this.EmpID = id;    
+	}    
+	public String getName() {    
+	    return EmpName;    
+	}    
+	public void setName(String Name) {    
+	    this.EmpName = Name;    
+	}    
+	public String getPassword() {    
+	    return EmpPassword;    
+	}    
+	public void setPassword(String Password) {    
+	    this.EmpPassword = Password;    
+	}
+	 
+
+}
